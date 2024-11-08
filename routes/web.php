e9c7829b2831a029d4c5ff0b05c3eb36f42cabdf
Gumiho2504 <hemchanmetrey123@gmail.com>
2024-11-08 16:27:26 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WelcomController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomController::class, 'welcome'])->name('welcome');
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// for notes
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +29,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
